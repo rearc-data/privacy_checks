a5b2b105b16eeeda448468109bc9fb7b2e062fe5
Mark deGroat <mhdegroat@gmail.com>
2023-10-17 19:16:19 -0400

    Fixes for imports, optimization of runner class, verbose option added for silent run option and is true by default

--- privacy_checks/test.py
+++ privacy_checks/test.py
@@ -10,5 +10,6 @@ df = pd.DataFrame(
     }
 )
 # Create a PrivacyRunner object
-runner = PrivacyRunner(checkers=CheckerSuites.full_privacy_suite(qi=['user','age'], sa=['disease']), dry_run=True, full_suite_run=True)
+
+runner = PrivacyRunner(checkers=CheckerSuites.full_privacy_suite(qi=['user','age'], sa=['disease']), dry_run=True, full_suite_run=True, verbose=True)
 runner.evaluate_data(df)
