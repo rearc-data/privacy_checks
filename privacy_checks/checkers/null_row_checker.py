e33676d952b2e76c8d41e07b479dce930b96f65f
Mark deGroat <mhdegroat@gmail.com>
2023-10-30 21:09:16 +0000

    Refactor null row checker with simpler, pandas based logic

--- privacy_checks/checkers/null_row_checker.py
+++ privacy_checks/checkers/null_row_checker.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import json
-from dataprofiler import Data, Profiler
 from .basic_checker import BasicChecker
 
 class NullRowChecker(BasicChecker):
@@ -8,49 +6,23 @@ class NullRowChecker(BasicChecker):
         self
     ):
         super().__init__('Null Row Checker')
-        # if not isinstance(df, pd.DataFrame):
-        #     raise TypeError("df must be a pandas DataFrame.")
-
-        # Assign values to instance variables
-        # self.df = df
-        # self.data = Data(data=df, data_type='csv')
-        self.profile = None
-        self.report = None
-
-    def generate_report(self, data):
-        # Perform k-anonymity check on the DataFrame
-        if (not self.profile):
-            self.profile = Profiler(data)
-        if (not self.report):
-            self.report = self.profile.report(
-                report_options={'output_format': 'pretty'})
-        return self.report
-
-    def null_row_check(self, df: pd.DataFrame):
-        data = Data(data=df, data_type='csv')
-        if (not self.report):
-            self.generate_report(data)
-        null_threshold = 0
-        if self.report['global_stats']['row_is_null_ratio'] > null_threshold:
-            # print("Null rows found in dataset")
-            # raise Exception("Detected null rows within data set")
+            
+    def check_dataset(self, df):
+        null_rows = df.isnull().any(axis=1)
+        null_row_count = null_rows.sum()
+        if null_rows.any():
             return {
                 'message': 'Null Row check has failed.',
                 'title': 'Null Row control',
                 'status': False,
-                'value': self.report['global_stats']['row_is_null_ratio'],
-                'threshold': null_threshold
+                'value': null_row_count,
+                'threshold': 0
             }
         else:
-            # print("No null rows found in dataset")
-            #return (True, 0, 'No null rows found in dataset')
             return {
                 'message': 'No null rows found in dataset.',
                 'title': 'Null Row control',
                 'status': True,
-                'value': self.report['global_stats']['row_is_null_ratio'],
-                'threshold': null_threshold
+                'value': null_row_count,
+                'threshold': 0
             }
-            
-    def check_dataset(self, df):
-        return self.null_row_check(df)
